8e5ddd2c66cd0b2fd8a71d01fac4cbed9b0883d0
Rubik009 <romanrubbk96@mail.ru>
2022-04-29 21:42:17 +0300

    work with ElephantSQL

--- config/database.js
+++ config/database.js
@@ -1,11 +1,19 @@
-const Pool = require('pg').Pool;
+const pg = require('pg');
 
-const pool = new Pool({
-    user : 'postgres',
-    password : '12345',
-    database : 'todolist_postgres',
-    host : 'localhost',
-    port : '5432'
+var client = new pg.Client("postgres://pvnhzcrj:jC7pqIy-pSXe77IwU3sEvIcGH0PBRWYP@chunee.db.elephantsql.com/pvnhzcrj");
+
+client.connect(function(err) {
+  if(err) {
+    return console.error('could not connect to postgres', err);
+  }
+  client.query('SELECT NOW() AS "theTime"', function(err, result) {
+    if(err) {
+      return console.error('error running query', err);
+    }
+    console.log(result.rows[0].theTime);
+    // >> output: 2018-08-23T14:02:57.117Z
+  });
+
 });
 
-module.exports = pool;
+module.exports = client;
--- index.js
+++ index.js
@@ -2,12 +2,13 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 require('dotenv').config();
-const sequelize = require('sequelize');
+const db = require('./config/database');
 const routes = require("./routes/index");
 
 
 app.use(bodyParser.json());
 app.use("/api", routes);
+
 
 const server = app.listen(process.env.PORT, () => console.log('Server started'));
 
